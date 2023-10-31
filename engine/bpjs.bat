adf2596beb93fbb61e4ba7a98ee37eb5f22289c1
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2023-10-31 09:14:23 +0200

    Changed ess to use PrioritizedBSyncEventSelectionStrategy by default

--- engine/bpjs.bat
+++ engine/bpjs.bat
@@ -1,3 +1,3 @@
 @echo off
-SET jar=%~dp0%BPjs-Context-0.6.10.uber.jar
+SET jar=%~dp0%BPjs-Context-0.6.12.uber.jar
 java -jar %jar% %*
--- engine/bpjs.sh
+++ engine/bpjs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-UBERJAR=$SCRIPT_DIR/BPjs-Context-0.6.10.uber.jar
+UBERJAR=$SCRIPT_DIR/BPjs-Context-0.6.12.uber.jar
 if [ ! -f $UBERJAR ]; then
     echo Missing jar file $UBERJAR in the target directory
     exit -2
