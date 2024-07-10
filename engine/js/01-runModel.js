dcc0f80db4aedc758e2bd79c1c3834ee6603ca40
Achiya Elyasaf <10044875+achiyae@users.noreply.github.com>
2024-07-10 22:44:42 +0300

    Add support for groups

--- engine/js/01-runModel.js
+++ engine/js/01-runModel.js
@@ -10,16 +10,19 @@ function cloneToken(token) {
 var nodes = new Map();
 var starts = [];
 var disabledTabs = [];
-var groups = {};
+var groups = new Map();
+
+// init tabs
 for (let n of model.config.flows) {
   if (n.type === 'tab' && n.disabled) {
     disabledTabs.push(n.id);
-  } else if (n.type === 'group') {
-    groups[n.id] = n;
   }
 }
+
+// init nodes
 for (let n of model.config.flows) {
   if (!disabledTabs.includes(n.z) && !n.d) {
+    n.inNodes = new Set();
     nodes.set(n.id, n)
     if (n.type == "start") {
       let token = n.payload || "{}";
@@ -28,10 +31,52 @@ for (let n of model.config.flows) {
     }
   }
 }
+
+// init nodes inNodes
+for (let n of nodes.values()) {
+  if (n.wires) {
+    for (let port of n.wires) {
+      for (let o of port) {
+        nodes.get(o).inNodes.add(n.id);
+      }
+    }
+  }
+}
+
+// init groups
+for (let n of model.config.flows) {
+  if (!disabledTabs.includes(n.z) && n.type === 'group') {
+    groups.set(n.id, n);
+  }
+}
+
+// init nodes inGroups
+for (let g of groups.values()) {
+  g.rwbi = { request: [], waitFor: [], block: [], interrupt: [] }
+  for (let id of g.nodes) {
+    let n = nodes.get(id);
+    if (n.inNodes.size === 0) {
+      if (n.type === 'bsync') {
+        throw new Error("bsync is still not supported inside a group")
+      } else if (n.eventType === 'request') {
+        g.rwbi.request.push(n.id)
+      } else if (n.eventType === 'waitFor') {
+        g.rwbi.waitFor.push(n.id)
+      } else if (n.eventType === 'block') {
+        g.rwbi.block.push(n.id)
+      } else if (n.eventType === 'interrupt') {
+        g.rwbi.interrupt.push(n.id)
+      } else {
+        throw new Error(`node cannot be inside a group: ${n}`)
+      }
+    }
+  }
+}
+
 //-------------------------------------------------------------------------------
 // Initial spawn
 //-------------------------------------------------------------------------------
-bthread("initial", function() {
+bthread("initial", function () {
   for (let n of starts) {
     if (RED.nodeRedAdapter) {
       RED.nodeRedAdapter.updateToken(n, JSON.parse(n.token), true);
@@ -85,24 +130,27 @@ function spawn_bthread(node, token) {
 function execute(node, token) {
   let cloneToken = JSON.parse(JSON.stringify(token))
   let event;
-  let block = [];
-  let waitFor = [];
+  let block = 0, waitFor = 0, request = 0, interrupt = 0;
   try {
     if (node.g) {
-      let name = groups[node.g].name
-      let matches = name.match(/Block *:? *\[([^\]]*).*break[ \-_]upon *:? *\[([^\]]*)/i)
-      if (!matches) {
-        throw new Error("A group name must be:\nblock: [<a comma separated list of blocks' names>] | break-upon: [<a comma separated list of blocks' names>]")
-      }
-      block = matches[1].split(',').map(v => v.trim()).map(v => v.replace(/"/g, '')).filter(v => v.length > 0)
-      waitFor = matches[2].split(',').map(v => v.trim()).map(v => v.replace(/"/g, '')).filter(v => v.length > 0)
+      let group = groups.get(node.g)
 
-      for (let i = 0; i < block.length; i++) {
-        bp.thread.data.block.push(Any(block[i]));
-      }
-      for (let i = 0; i < waitFor.length; i++) {
-        bp.thread.data.waitFor.push(Any(waitFor[i]));
-      }
+      group.rwbi.block.map(id => nodes.get(id)).forEach(n => {
+        bp.thread.data.block.push(defaultEventDefinition(n, cloneToken))
+        block++
+      });
+      group.rwbi.waitFor.map(id => nodes.get(id)).forEach(n => {
+        bp.thread.data.waitFor.push(defaultEventDefinition(n, cloneToken))
+        waitFor++
+      });
+      group.rwbi.request.map(id => nodes.get(id)).forEach(n => {
+        bp.thread.data.request.push(defaultEventDefinition(n, cloneToken))
+        request++
+      });
+      group.rwbi.interrupt.map(id => nodes.get(id)).forEach(n => {
+        bp.thread.data.interrupt.push(defaultEventDefinition(n, cloneToken))
+        interrupt++
+      });
     }
     switch (node.type) {
       //-----------------------------------------------------------------------
@@ -112,13 +160,13 @@ function execute(node, token) {
         return [cloneToken]
 
       case "switch":
-        return switchNode(node,cloneToken)
+        return switchNode(node, cloneToken)
       case "log":
-        if(node.level === 'info')
+        if (node.level === 'info')
           bp.log.info(cloneToken)
-        else if(node.level === 'warn')
+        else if (node.level === 'warn')
           bp.log.warn(cloneToken)
-        if(node.level === 'fine')
+        if (node.level === 'fine')
           bp.log.fine(cloneToken)
         return []
       case "loop":
@@ -173,32 +221,39 @@ function execute(node, token) {
         }
 
         event = sync(stmt)
-        cloneToken.selectedEvent = {name: String(event.name)}
-        if (event.data != null) cloneToken.selectedEvent.data = event.data
+        copyEventDataToToken(cloneToken, event)
         return [cloneToken]
       default:
         if (this[node.type]) {
           this[node.type](node, cloneToken)
         } else {
           if (node.eventType == 'request') {
-            defaultRequestEventDef(node, cloneToken);
+            event = sync({ request: defaultEventDefinition(node, cloneToken) })
+            copyEventDataToToken(cloneToken, event)
           } else if (node.eventType == 'waitFor') {
-            defaultWaitForEventDef(node, cloneToken);
+            event = sync({ waitFor: defaultEventSetDefinition(node, cloneToken)})
+            copyEventDataToToken(cloneToken, event)
           }
         }
         return [cloneToken]
     }
   } finally {
-    for (let i = 0; i < block.length; i++) {
+    for (let i = 0; i < block; i++) {
       bp.thread.data.block.pop()
     }
-    for (let i = 0; i < waitFor.length; i++) {
+    for (let i = 0; i < waitFor; i++) {
       bp.thread.data.waitFor.pop()
+    }
+    for (let i = 0; i < request; i++) {
+      bp.thread.data.request.pop()
+    }
+    for (let i = 0; i < interrupt; i++) {
+      bp.thread.data.interrupt.pop()
     }
   }
 }
 
-function defaultWaitForEventDef(node, msg) {
+function defaultEventSetDefinition(node, msg) {
   function conditionForField(msg, node, field) {
     let target = '';
     // bp.log.info("res node={0};msg={1}; field={2}", node,msg, field);
@@ -228,11 +283,10 @@ function defaultWaitForEventDef(node, msg) {
   }
   condition += ' })'
   // bp.log.info("condition="+condition)
-  let event = sync({waitFor: eval(condition)})
-  copyEventDataToToken(msg, event)
+  return eval(condition)
 }
 
-function defaultRequestEventDef(node, msg) {
+function defaultEventDefinition(node, msg) {
   function setField(msg, node, field, target) {
     // bp.log.info("res node={0};msg={1}; field={2}", node,msg, field);
     if (msg[node.type] && msg[node.type][field.name]) {
@@ -262,17 +316,11 @@ function defaultRequestEventDef(node, msg) {
   } else {
     event = bp.Event(String(node.type))
   }
-
-  let stmt = {}
-  stmt[node.eventType] = event
-  // bp.log.warn(node.type + " was called, but the method does not exists.")
-  // bp.log.info("asking for {0}, bp.thread.data={1}", stmt, bp.thread.data)
-  sync(stmt)
-  copyEventDataToToken(msg, event)
+  return event;
 }
 
 function copyEventDataToToken(token, event) {
-  token.selectedEvent = {name: String(event.name)}
+  token.selectedEvent = { name: String(event.name) }
   if (event.data != null) {
     if (typeof event.data === 'object') {
       if (!token[event.name]) {
